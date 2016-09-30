0c852488671bc9c7b4c2e38da2a3bdd48808c88c
Andrew Yeung <andrewhyeung@gmail.com>
2016-09-30 08:41:38 -0700

    added quicker bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,20 +1,37 @@
 // sort the list in ascending order, swapping two at a time.
 
+// function bubbleSort(arr){
+// 	for(var x = 0; x<arr.length; x++){
+// 		for(var i = 1; i < arr.length; i++){
+// 			if(arr[i-1] > arr[i]){
+// 				var temp = arr[i]; 
+// 				arr[i] = arr[i-1]; 
+// 				arr[i-1] =temp; 
+// 			}
+// 		}	
+// 	}
+	
+// 	return arr; 
+// }
+//slow implementation
 function bubbleSort(arr){
-	for(var x = 0; x<arr.length; x++){
+	var swapped = true; 
+	while(swapped === true){
+		swapped = false; 
 		for(var i = 1; i < arr.length; i++){
 			if(arr[i-1] > arr[i]){
 				var temp = arr[i]; 
 				arr[i] = arr[i-1]; 
 				arr[i-1] =temp; 
+				swapped = true;
 			}
 		}	
 	}
-	
 	return arr; 
 }
 
-//extre
+
 var list = [3,2,9,6,5]; 
 var list2 = [6,5,4,3,2,1]
-console.log(bubbleSort(list2))
+console.log(bubbleSort(list))
+
