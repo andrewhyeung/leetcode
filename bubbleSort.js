acbec4dab103997e370b32d2e183f0c858843619
Andrew Yeung <andrewhyeung@gmail.com>
2016-11-21 22:52:14 -0800

    longest palindrome

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,43 +1,8 @@
 // sort the list in ascending order, swapping two at a time.
 
-// function bubbleSort(arr){
-// 	for(var x = 0; x<arr.length; x++){
-// 		for(var i = 1; i < arr.length; i++){
-// 			if(arr[i-1] > arr[i]){
-// 				var temp = arr[i]; 
-// 				arr[i] = arr[i-1]; 
-// 				arr[i-1] =temp; 
-// 			}
-// 		}	
-// 	}
-	
-// 	return arr; 
-// }
-//slow implementation
-var count = 0, maxCount =100; 
-
-console.log(count); 
-console.log(maxCount); 
-// function bubbleSort(arr){
-// 	var count = 0; 
-// 	var swapped = true; 
-// 	while(swapped === true){
-// 		swapped = false; 
-// 		for(var i = 1; i < arr.length - count; i++){
-// 			if(arr[i-1] > arr[i]){
-// 				var temp = arr[i]; 
-// 				arr[i] = arr[i-1]; 
-// 				arr[i-1] =temp; 
-// 				swapped = true;
-// 			}
-// 		}	
-// 		count++; 
-// 	}
-// 	return arr; 
-// }
-
-
-// var list = [3,2,9,6,5]; 
-// var list2 = [6,5,4,3,2,1]
-// console.log(bubbleSort(list))
+var list = [5,1,3,11,5]; 
+function bubbleSort(){
+	var swapped = 1
+	while(swapped )
+}
 
